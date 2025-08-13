5ee9f78dae7e77977437de954c08677f41c49e3e
Lucas dos Santos Ramos <ramos.lucas@ufsc.br>
2025-08-13 20:03:52 -0300

    feat: add espiral quadrada

---
+++ 02_espiral_quadrada/espiral_quadrada.py
@@ -0,0 +1,16 @@
+import turtle
+
+
+construtor = turtle.Turtle()
+construtor.speed(20)
+
+initial_size = 50
+size_increase = 20
+num_turns = 50
+
+for size in range(initial_size, initial_size + size_increase*num_turns, size_increase):
+
+    construtor.forward(size)
+    construtor.right(90)
+
+turtle.done()
