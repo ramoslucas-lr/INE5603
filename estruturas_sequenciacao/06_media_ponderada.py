b707de6e917ee545a291d2627ae89ca0b6dc4e04
Lucas Ramos <ramoslucas.lr@gmail.com>
2025-08-27 15:26:34 -0300

    feat: add estruturas_sequenciacao folder

---
+++ estruturas_sequenciacao/06_media_ponderada.py
@@ -0,0 +1,11 @@
+n1 = float(input())
+n2 = float(input())
+n3 = float(input())
+
+p1 = 2
+p2 = 3
+p3 = 5
+
+media = (n1*p1+n2*p2+n3*p3)/(p1+p2+p3)
+
+print("MEDIA =", round(media, 1))
---
+++ estruturas_sequenciacao/10_aprovacao_aluno.py
@@ -0,0 +1,11 @@
+n1 = float(input())
+n2 = float(input())
+n3 = float(input())
+
+p1 = float(input())
+p2 = float(input())
+p3 = float(input())
+
+media = (n1*p1+n2*p2+n3*p3)/(p1+p2+p3)
+
+print(media>=6)
